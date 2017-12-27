a8f3eb565de044a4233f3d5a28a74e7abfc12de7
Anderson Lobo Feitosa <andersonlf@gmail.com>
2017-12-26 22:43:32 -0200

    fb2

---
+++ src/test/java/com/andersonlfeitosa/buildpipeline/ApiControllerTest2.java
@@ -0,0 +1,13 @@
+package com.andersonlfeitosa.buildpipeline;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ApiControllerTest2 {
+
+  @Test
+  public void sayHello() {
+    Assert.assertEquals("Hello World", "Hello World");
+  }
+
+}
