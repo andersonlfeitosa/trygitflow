abccba86768c6f361d394016235e19e5e55c5aea
Anderson Lobo Feitosa <andersonlf@gmail.com>
2018-01-01 08:11:18 -0200

    FB2

---
+++ src/main/java/com/andersonlfeitosa/buildpipeline/FB2.java
@@ -0,0 +1,26 @@
+package com.andersonlfeitosa.buildpipeline;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Endpoint responsável por exibir mensagem. 
+ * 
+ * @author Anderson Lobo Feitosa - andersonlf@gmail.com
+ * @since 2017-12-11
+ * @version 1.0.0
+ */
+@RestController
+public class FB2 {
+
+  /**
+   * Expõe método HTTP GET para exibição de mensagem.
+   *  
+   * @return A mensagem.
+   */
+  @RequestMapping("/fb2")
+  public String sayHello() {
+    return ("Hello World, this is a SpringBoot Microservice");
+  }
+
+}
